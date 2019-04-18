5cb0ebfee85bc393ed08a3b28b2bc312e329b004
Rageeb Mahtab <rmahtab0079@bths.edu>
2019-04-18 17:29:00 -0400

    All Screens and Classes are added

--- froot-loops/View Controllers/HomeViewController.swift
+++ froot-loops/View Controllers/HomeViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import GoogleMaps
 
 class HomeViewController: UIViewController {
 
@@ -21,20 +20,6 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    override func loadView() {
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: 40.7128, longitude: 74.0060, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = mapView
-        
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: 40.7128, longitude: 74.0060)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
-    }
     
     @IBAction func onSwitchSegment(_ sender: Any) {
         let getIndex = tableMapSegmentControl.selectedSegmentIndex
