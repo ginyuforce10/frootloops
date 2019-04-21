ab3a07aa563aab94cd997d7dd95aaab2898142f5
hchowdhury3273 <hc2324@nyu.edu>
2019-04-21 02:54:08 -0400

    test

--- froot-loops/View Controllers/MyEventsViewController.swift
+++ froot-loops/View Controllers/MyEventsViewController.swift
@@ -58,15 +58,28 @@ class MyEventsViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+       // let movie = movies[indexPath.row]
+        
+        let detailsViewController = segue.destination as! EventDetailViewController
+        
+        detailsViewController.etitle = titles[indexPath.row]
+        detailsViewController.eimage = images[indexPath.row]
+        detailsViewController.edescription = descriptions[indexPath.row]
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
     }
-    */
+    
     
 
 
